7d13ccd0d81e85a86bb15b3c4ee45038e6958174
Your Name <zingorskiy@gmail.ru>
2024-07-21 11:31:37 +0300

    the generator functions and their tests have been redesigned

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,7 +1,93 @@
-from src.generators import filter_by_currency, transaction_descriptions
+import pytest
 
-transactions = [
-    {
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+
+
+@pytest.fixture
+def transactions():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",  # состояние, статус
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {  # сумма операции
+                "amount": "9824.07",  # количество, сумма
+                "currency": {"name": "USD", "code": "USD"},  # валюта  # имя  # код
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+@pytest.fixture
+def start():
+    return 1
+
+
+@pytest.fixture
+def end():
+    return 1
+
+
+@pytest.fixture
+def currency():
+    return "USD"
+
+
+def test_filter_by_currency(transactions, currency):
+    # тестирование фильтра валютной операции
+    result = next(filter_by_currency(transactions, currency))
+    assert result == {
         "id": 939719570,
         "state": "EXECUTED",
         "date": "2018-06-30T02:08:58.425572",
@@ -9,86 +95,23 @@ transactions = [
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
         "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
+    }
 
 
-def test_transaction_descriptions():
-    generator = transaction_descriptions(transactions)
-    assert next(generator) == "Перевод организации"
-    assert next(generator) == "Перевод со счета на счет"
-    assert next(generator) == "Перевод со счета на счет"
-    assert next(generator) == "Перевод с карты на карту"
-    assert next(generator) == "Перевод организации"
+def test_transaction_descriptions(transactions):
+    # тестирование функции описания
+    descriptions = transaction_descriptions(transactions)
+    expected_descriptions = [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
+    ]
+    for expected in expected_descriptions:
+        assert next(descriptions) == expected
 
 
-# @pytest.mark.parametrize("currency", [("USD", [939719570, 142264268, 895315941]),
-#                                                ("RUB", [873106923, 594226727])])
-def test_filter_by_currency():
-    generator = filter_by_currency(transactions, "USD")
-    assert next(generator) == 939719570
-    assert next(generator) == 142264268
-    assert next(generator) == 895315941
-    generator_1 = filter_by_currency(transactions, "RUB")
-    assert next(generator_1) == 873106923
-    assert next(generator_1) == 594226727
-
-
-"""
-def filter_by_currency(transactions: list, currency: str) -> Generator:
-
-    for transaction in transactions:
-        operation_amount_dic = transaction["operationAmount"]
-        currency_dic = operation_amount_dic["currency"]
-        if currency_dic["code"] == currency:
-            yield transaction["id"]
-"""
-"""
-def test_infinite_sequence():
-    generator = infinite_sequence()
-    assert next(generator) == 1
-    assert next(generator) == 2
-    assert next(generator) == 3
-@pytest.fixture
-def account_number():
-    return 3243213432186753213
-@pytest.mark.parametrize("expected")
-def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == "1324 32** **** 3213"
-"""
+def test_card_number_generator(start, end):
+    number = next(card_number_generator(start, end))
+    assert number == "0000 0000 0000 0001"
