1635b9f2355f189ffb493e9ee66c44ddc12de784
Your Name <zingorskiy@gmail.ru>
2024-08-01 19:00:30 +0300

    fix decorator and fix test 3.2

--- src/decorators.py
+++ src/decorators.py
@@ -11,25 +11,31 @@ def log(filename: Any) -> Callable:
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                log_message = f"{func.__name__} called with args: {args}, kwargs:{kwargs}. Result: {result}"
+                if filename is not None:
+                    with open(filename, "w") as file:
+                        file.write("my function OK")
+                else:
+                    print("my function OK")
+                return result
             except Exception as e:
-                log_message = f"{func.__name__} error: {e}. Inputs:{args}, {kwargs}"
-            if filename:
-                with open(filename, "a", encoding="utf-8") as file:
-                    file.write(log_message + "\n")
-                    print(log_message)
-            else:
-                print(log_message)
-#            return result
+                if filename is not None:
+                    with open(filename, "w") as file:
+                        file.write(f"my function error:{e}. Input: {args, kwargs}")
+                # else: Не знаю нужно это или нет, на всякий случай(так и непонял что должно выводиться при ошибке)
+                # print(f"Функция не отрабатывает - введдите корректные данные: Ошибка {e}")
+                # return f"Функция не отрабатывает - введдите корректные данные: Ошибка {e}"
+                # return Exception("f"Функция не отрабатывает - введдите корректные данные: Ошибка {e}"")
+                return func(*args, **kwargs)
+
         return wrapper
 
     return decorator
 
 
 @log(filename="test_log.txt")
-#@log(filename="")
+# @log(filename="")
 def my_function(x: int, y: int) -> int:
     return x + y
 
 
-#my_function(1,2)
+# my_function(1,2)
